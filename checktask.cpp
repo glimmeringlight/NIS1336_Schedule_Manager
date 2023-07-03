de618f8fa16e3bc92a23d82d907f5626e9c01b8a
glimmeringlight <3137181992@qq.com>
2023-07-03 15:05:51 +0800

    Use struct tm

---
+++ checktask.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"types.h"
+
+
+void checkTask(){// check arriving task every minute
+
+}
--- types.h
+++ types.h
@@ -12,10 +12,10 @@ typedef struct
 {
     int id;
     char name[32];
-    time_t s_time;  //任务启动时间
+    struct tm s_time;  //任务启动时间
     Priority prio;  //任务优先级
     Category cat;   //类别
-    time_t rem; //reminder, 提醒时间
+    struct tm rem; //reminder, 提醒时间
     char detail[256];   //任务详细信息
 }Task;
 

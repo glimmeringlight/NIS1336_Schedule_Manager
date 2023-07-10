55b1eb4a37def3bf6ffa1dec4915ad5572bd50e0
glimmeringlight <3137181992@qq.com>
2023-07-10 23:14:13 +0800

    Add alt task; Enhancement: Check time format

--- src/cli.cpp
+++ src/cli.cpp
@@ -7,17 +7,20 @@
 #include<iomanip>
 #include<unistd.h>
 #include<iostream>
+#include<regex>
 #include "Tasks.h"
 
 void showTask(int argc, char * argv[]);
 void addTask(int argc, char * argv[]);
 void delTask(int argc, char * argv[]);
+void altTask(int argc, char * argv[]);
 void userLogin(int argc, char * argv[]);
 void userRegister(int argc, char * argv[]);
 void changePasswd(int argc, char * argv[]);
 void checkTask(int argc, char* argv[]);
 void cmdError();
 void showUsage();
+bool checkDateFormat(const std::string& str);
 
 int main(int argc, char * argv[]){
     /*
@@ -40,6 +43,7 @@ int main(int argc, char * argv[]){
     if(!strncmp(argv[1], "reg", 3)) op = 6;
     if(!strncmp(argv[1], "passwd", 6)) op = 7;
     if(!strncmp(argv[1], "check", 5)) op = 8;
+    if(!strncmp(argv[1], "alt", 3)) op = 9;
 
     switch (op)
     {
@@ -75,6 +79,9 @@ int main(int argc, char * argv[]){
         checkTask(argc, argv);
         break;
 
+    case 9:
+        altTask(argc, argv);
+        break;
 
     default:
         cmdError();
@@ -280,6 +287,11 @@ void addTask(int argc, char * argv[]){
     std::string r_time_str = rem;
     std::string s_time_str = st;
 
+    if(!(checkDateFormat(r_time_str) && checkDateFormat(s_time_str))){
+        printf("Time format error!\n");
+        exit(-1);
+    }
+
     tm r_time_tm = {};
     tm s_time_tm = {};
 
@@ -303,6 +315,13 @@ void addTask(int argc, char * argv[]){
         exit(-1);
     }
 
+    for(auto it = tasklist.begin(); it != tasklist.end(); ++it){
+        if(it->s_time == new_task.s_time){
+            printf("This start time is already occupied!\n");
+            exit(-1);
+        }
+    }
+
     //add new elements
     tasklist.push_back(new_task);
 
@@ -310,6 +329,195 @@ void addTask(int argc, char * argv[]){
 
 
 
+
+
+}
+
+void altTask(int argc, char * argv[]){
+    /*
+    ./cli alt -u username -p password -i id -n name -o prio -c cat -r rem -d detail -s start
+    */
+
+    Account account("USER_PWD.txt");
+
+    //login info
+    char* username = NULL;
+    char* password = NULL;
+    int id = 0;
+    char* name = NULL;
+    char* detail = NULL;
+    char* c_prio = NULL;
+    char* c_cat = NULL;
+    char* c_rem = NULL;
+    char* c_stime = NULL;
+
+
+
+
+    char optret;
+    
+    while((optret = getopt(argc, argv, "u:p:i:n:o:c:r:d:s:"))!=-1){
+        switch (optret)
+        {
+        case 'u':
+            username = optarg;
+            break;
+
+        case 'p':
+            password = optarg;
+            break;
+
+        case 'i':
+            id = atoi(optarg);
+            break;
+
+        case 'n':
+            name = optarg;
+            break;
+
+        case 'o':
+            c_prio = optarg;
+            break;
+
+        case 'c':
+            c_cat = optarg;
+            break;
+
+        case 'r':
+            c_rem = optarg;
+            break;
+
+        case 'd':
+            detail = optarg;
+            break;
+
+        case 's':
+            c_stime = optarg;
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    //check login info
+    if(username == NULL || password == NULL){
+        printf("Too few arguments!\n");
+        exit(-1);
+    }
+
+    User user = account.login(username, password);
+    if(user.id == 0){
+        printf("User auth failed!\n");
+        exit(-1);
+    }
+
+    std::vector<Task> tasklist;
+    loadTask(tasklist, &user);
+
+    std::vector<Task>::iterator target_it;
+    bool found = false;
+    for(target_it = tasklist.begin(); target_it != tasklist.end(); ++target_it){
+        if(target_it->id == id){
+            found = true;
+            break;
+        }
+    }
+
+    if(!found){
+        printf("Cannot found task %d.\n", id);
+        exit(-1);
+    }
+
+    //check name
+    if(name){
+        bool name_existed = false;
+        for(auto it = tasklist.begin(); it != tasklist.end(); ++it){
+            if(!strcmp(it->name, name)){
+                name_existed = true;
+                break;
+            }
+        }
+
+        if(name_existed){
+            printf("Task name already exists!\n");
+            exit(-1);
+        }
+
+        strcpy(target_it->name, name);
+        
+    }
+    //check detail
+    if(detail){
+        strcpy(target_it->detail, detail);
+    }
+    //check prio
+    if(c_prio){
+        std::string s_prio = c_prio;
+        target_it->prio = convertToPriority(s_prio);
+    }
+    //check category
+    if(c_cat){
+        std::string s_cat = c_cat;
+        target_it->cat = convertToCategory(s_cat);
+    }
+
+    //check s_time and rem_time
+    time_t stime;
+    time_t rtime;
+    if(c_stime){
+        std::string s_time_str = c_stime;
+        if(!checkDateFormat(s_time_str)){
+            printf("Time format error!\n");
+            exit(-1);
+        }
+        tm s_time_tm = {};
+        std::istringstream ss(s_time_str);
+        ss >> std::get_time(&s_time_tm, "%Y-%m-%d,%T");
+        stime = mktime(&s_time_tm);
+    }else{
+        stime = target_it->s_time;
+    }
+
+    if(c_rem){
+        std::string r_time_str = c_rem;
+        if(!checkDateFormat(r_time_str)){
+            printf("Time format error!\n");
+            exit(-1);
+        }
+        tm r_time_tm = {};
+        std::istringstream sr(r_time_str);
+        sr >> std::get_time(&r_time_tm, "%Y-%m-%d,%T");
+        rtime = mktime(&r_time_tm);
+    }else{
+        rtime = target_it->rem;
+    }
+
+    //validation
+    time_t cur;
+    time(&cur);
+
+    if(cur > stime || cur > rtime){
+        printf("Cannot use past time!\n");
+        exit(-1);
+    }
+
+    if(stime < rtime){
+        printf("Start time cannot be earlier than remind time.\n");
+        exit(-1);
+    }
+
+    for(auto it = tasklist.begin(); it != tasklist.end();++it){
+        if(it->s_time == stime){
+            printf("This start time is occupied!\n");
+            exit(-1);
+        }
+    }
+
+    target_it->rem = rtime;
+    target_it->s_time = stime;
+
+    saveTask(tasklist, &user);
 
 
 }
@@ -567,6 +775,13 @@ void showUsage(){
     printf("./cli passwd -u username -p password -n newpassword\n");
     printf("./cli check -u username -p password.\n");
     
-    
-    
+}
+
+
+bool checkDateFormat(const std::string& str) {
+    // 定义正则表达式
+    std::regex re(R"(^\d{4}-\d{2}-\d{2},\d{2}:\d{2}:\d{2}$)");
+
+    // 判断是否匹配正则表达式
+    return std::regex_match(str, re);
 }

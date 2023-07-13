704a9007721433677845c056cd8ca87f3d9003fe
felixchen0707 <3137181992@qq.com>
2023-07-13 23:06:48 +0800

    Final commit

--- scripts/test.sh
+++ scripts/test.sh
@@ -20,6 +20,8 @@ echo "[Task 2] Testing basic function."
 echo "[2.1] Add a new task"
 ./cli add -u test -p newpwd -n TestTask -o HIGH -c LIFE -r 2025-01-01,00:00:00 -s 2025-01-02,00:00:00 -d "Just for test"
 ./cli show -u test -p newpwd
+echo "[2.2] Modify an existing task."
+./cli alt -u test -p newpwd -i 1 -n Newname
 echo "[2.2] Add a task which has existing name"
 ./cli add -u test -p newpwd -n TestTask -o HIGH -c LIFE -r 2025-01-01,00:00:00 -s 2025-01-02,00:00:00 -d "Just for test2"
 echo "[2.3] Add a task which has invalid time"
